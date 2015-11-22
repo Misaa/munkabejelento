8bd85195846ad1b02434419a962642900b7d0c21
misaa <ovaritimi@gmail.com>
2015-11-22 20:20:26 +0000

    teszt

--- controllers/index.test.js
+++ controllers/index.test.js
@@ -1,7 +1,8 @@
-
+// Kezdeti lépések
 var Browser = require('zombie');
-
 Browser.localhost(process.env.IP, process.env.PORT);
+var browser = new Browser();
+
 
 describe('User visits index page', function() {
     var browser = new Browser();
@@ -15,7 +16,72 @@ describe('User visits index page', function() {
     });
     
     it('should see welcome page', function() {
-        browser.assert.text('div.page-header > h1', 'Munkabejelentés');
+        browser.assert.text('div.page-header > h1', 'Munkabejelentő oldal');
     });
 });
 
+//Hitelesítés
+describe('User visits new timesheet page', function (argument) {
+    var browser = new Browser();
+    before(function() {
+        return browser.visit('/timesheets/new');
+    });
+    
+    it('should go to the authentication page', function () {
+        browser.assert.redirected();
+        browser.assert.success();
+        browser.assert.url({ pathname: '/login' });
+    });
+    
+    it('should be able to login with correct credentials', function (done) {
+        browser
+            .fill('email', 'k81dnc')
+            .fill('password', 'jelszo')
+            .pressButton('button[type=submit]')
+            .then(function () {
+                browser.assert.redirected();
+                browser.assert.success();
+                browser.assert.url({ pathname: '/timesheets/list' });
+                done();
+            })
+            
+    });
+});
+
+it('should go the timesheet page', function () {
+    return browser.visit('/timesheets/new')
+    .then(function () {
+        browser.assert.success();
+        browser.assert.text('div.page-header > h1', 'Bejelentkezés');
+    });
+});
+
+it('should show errors if the form fields are not right', function () {
+    return browser
+        .fill('helyszin', '')
+        .fill('leiras', '')
+        .pressButton('button[type=submit]')
+        .then(function() {
+            // browser.assert.redirected();
+            browser.assert.success();
+            browser.assert.element('form .form-group:nth-child(1) [name=helyszin]');
+            browser.assert.hasClass('form .form-group:nth-child(1)', 'has-error');
+        });
+});
+
+it('should show submit the right-filled form fields and go back to list page', function() {
+    browser
+        .fill('helyszin', 'cica')
+        .fill('leiras', 'kutya')
+        .pressButton('button[type=submit]')
+        .then(function() {
+            browser.assert.redirected();
+            browser.assert.success();
+            browser.assert.url({ pathname: '/timesheets/list' });
+            
+            browser.assert.element('table.table');
+            browser.assert.text('table.table tbody tr:last-child td:nth-child(2) span.label', 'Új');    
+            browser.assert.text('table.table tbody tr:last-child td:nth-child(3)', 'cica');    
+            browser.assert.text('table.table tbody tr:last-child td:nth-child(4)', 'kutya');
+        });
+});
--- models/user.test.js
+++ models/user.test.js
@@ -1,4 +1,3 @@
-
 var expect = require("chai").expect;
 var bcrypt = require('bcryptjs');
 
@@ -26,64 +25,89 @@ before(function (done) {
 
 describe('UserModel', function () {
 
-    function getUserData() {
-        return {
-            email: 'abcdef',
-            password: 'jelszo',
-            surname: 'Gipsz',
-            forename: 'Jakab',
-            avatar: '',
-        };
-    }
-
     beforeEach(function (done) {
         User.destroy({}, function (err) {
             done();
         });
     });
     
-    it('should be able to create a user', function () {
-        return User.create({
-                email: 'abcdef',
-                password: 'jelszo',
-                surname: 'Gipsz',
-                forename: 'Jakab',
-                avatar: '',
-        })
-        .then(function (user) {
-            expect(user.email).to.equal('abcdef');
-            expect(bcrypt.compareSync('jelszo', user.password)).to.be.true;
-            expect(user.surname).to.equal('Gipsz');
-            expect(user.forename).to.equal('Jakab');
-            expect(user.avatar).to.equal('');
-        });
+    it('should work', function () {
+        expect(true).to.be.true;
     });
 
-    it('should be able to find a user', function() {
-        return User.create(getUserData())
-        .then(function(user) {
-            return User.findOneByEmail(user.email);
-        })
-        .then(function (user) {
-            expect(user.email).to.equal('abcdef');
-            expect(bcrypt.compareSync('jelszo', user.password)).to.be.true;
-            expect(user.surname).to.equal('Gipsz');
-            expect(user.forename).to.equal('Jakab');
-            expect(user.avatar).to.equal('');
-        });
-    });
+});
 
-    describe('#validPassword', function() {
-        it('should return true with right password', function() {
-             return User.create(getUserData()).then(function(user) {
-                 expect(user.validPassword('jelszo')).to.be.true;
-             })
-        });
-        it('should return false with wrong password', function() {
-             return User.create(getUserData()).then(function(user) {
-                 expect(user.validPassword('titkos')).to.be.false;
-             })
-        });
+//Felhasználó létrehozása
+it('should be able to create a user', function () {
+    return User.create({
+            email: 'abcdef',
+            password: 'jelszo',
+            surname: 'Gipsz',
+            forename: 'Jakab',
+    })
+    .then(function (user) {
+        expect(user.email).to.equal('abcdef');
+        expect(bcrypt.compareSync('jelszo', user.password)).to.be.true;
+        expect(user.surname).to.equal('Gipsz');
+        expect(user.forename).to.equal('Jakab');
     });
+});
 
+function getUserData() {
+    return {
+        email: 'abc',
+        password: 'jelszo',
+        surname: 'Gipsz',
+        forename: 'Jakab',
+    };
+}
+
+function getUserData2() {
+    return {
+        email: 'aaa',
+        password: 'jelszo',
+        surname: 'Gipsz',
+        forename: 'Jakab',
+    };
+}
+
+it('should be able to find a user', function() {
+    return User.create(getUserData())
+    .then(function(user) {
+        return User.findOneByEmail(user.email);
+    })
+    .then(function (user) {
+        expect(user.email).to.equal('abc');
+        expect(bcrypt.compareSync('jelszo', user.password)).to.be.true;
+        expect(user.surname).to.equal('Gipsz');
+        expect(user.forename).to.equal('Jakab');
+    });
+});
+
+[
+    {name: 'surname', value: ''},
+    {name: 'forename', value: ''},
+    {name: 'email', value: ''},
+    {name: 'password', value: ''},
+].forEach(function (attr) {
+    it('should throw error for invalid data: ' + attr.name, function () {
+        var userData = getUserData();
+
+        userData[attr.name] = attr.value;
+        
+        expect(User.create(userData)).to.throw;
+    });    
+});
+
+describe('#validPassword', function() {
+    it('should return true with right password', function() {
+         return User.create(getUserData()).then(function(user) {
+             expect(user.validPassword('jelszo')).to.be.true;
+         })
+    });
+    it('should return false with wrong password', function() {
+         return User.create(getUserData2()).then(function(user) {
+             expect(user.validPassword('titkos')).to.be.false;
+         })
+    });
 });
